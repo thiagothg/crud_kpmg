14f9525ada6a190f65a2905323aa0bf023c38537
Thiago Rodrigues <thiago_thg@outlook.com>
2022-03-01 17:05:29 -0300

    Factories & Seeders student

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'matricula' => $this->faker->randomNumber(9, false),
+            'cpf' => $this->faker->cpf,
+        ];
+    }
+}
